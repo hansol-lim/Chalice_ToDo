4bcb49b49f5627d2b438127b15fed20fb60f0492
hansol-lim <gksthf070@gmail.com>
2020-05-29 17:32:43 +0900

    Add: schema, put:avali

--- app.py
+++ app.py
@@ -9,20 +9,29 @@ def index():
 
 #Gets a list of all Todo's
 @app.route('/todos', methods=['GET'])
-def list():
+def get_list():
+    return {'hello': 'world'}
 
 #Create a new Todo
 @app.route('/todos', methods=['POST'])
-def list():
+def add_new_todo():
+    body = app.current_request.json_body
+    return get_app_db().add_item(
+        description=body['description'],
+        metadata=body.get('metadata')
+    )
 
 #Gets a specific Todo
 @app.route('/todos/{id}', methods=['GET'])
-def list():
+def get_todo():
+    return {'hello': 'world'}
 
 #Deletes a specific Todo
 @app.route('/todos/{id}', methods=['DELETE'])
-def list():
+def del_todo():
+    return {'hello': 'world'}
 
 #Updates the state of a Todo
 @app.route('/todos/{id}', methods=['PUT'])
-def list():
+def update_tod():
+    return {'hello': 'world'}
